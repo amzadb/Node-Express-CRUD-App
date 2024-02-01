2aba9236700e48e6f474e9887d56d634ada4b052
mallikapulagurtha <74366499+mallikapulagurtha@users.noreply.github.com>
2024-02-01 14:58:14 +0530

    Update routing.js

--- routes/routing.js
+++ routes/routing.js
@@ -32,22 +32,33 @@ router.post('/add', upload, async (req, resp) => {
             type: 'success',
             message: 'User added successfully!'
         };
+        req.session.username = new_user.name
         console.log(req.session.message);
-        resp.render('home', { title: "Home Page" });
+        console.log(req.session.username);
+        await req.session.save()
+        resp.redirect('/')
     } catch (saveError) {
         console.error('Error saving user:', saveError);
         return resp.status(500).json({ error: 'Error saving user' });
     }
 });
 
-router.get('/', (req, resp) => {
-    resp.render('home', { title: "Home Page" });
+router.get('/', async (req, resp) => {
+    try {
+        const users = await User.find({});
+        resp.render('home', {
+            title : "Home Page",
+            users: users
+        })
+      } catch (error) {
+        console.error('Error fetching members:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+      }
 });
 
 // router.get("/users", (req, resp) => {
 //     resp.send("All Users");
 // });
-
 router.get("/add", (req, resp) => {
     resp.render('add-user', { title: "Add User" })
 });
